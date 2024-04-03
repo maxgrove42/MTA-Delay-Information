284046f9a6bad31340fa5b7088086ad44a88f0cf
maxgrove42 <131721751+maxgrove42@users.noreply.github.com>
2024-04-03 18:29:55 -0400

    Maven Create

---
+++ mta-delays/src/test/java/com/groveenterprises/mta_delays/AppTest.java
@@ -0,0 +1,20 @@
+package com.groveenterprises.mta_delays;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
